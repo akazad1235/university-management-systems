dfcc5ec5a9554bcb8e927ae61083bbebc47ed8e7
Md Shakil Ahmed <shakilatrai5@gmail.com>
2023-05-20 21:37:27 +0600

    theme mastaring and login page added done

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,51 +1,66 @@
-{{-- <x-guest-layout>
-    <!-- Session Status -->
-    <x-auth-session-status class="mb-4" :status="session('status')" />
+@extends('layout.app')
 
-    <form method="POST" action="{{ route('login') }}">
-        @csrf
-
-        <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+@section('mainContent')
+    <div class="row flex-center min-vh-100 py-6">
+        <div class="col-sm-10 col-md-8 col-lg-6 col-xl-5 col-xxl-4">
+            <a class="d-flex flex-center mb-4" href="/">
+                <img class="me-2" src="../../../assets/img/icons/spot-illustrations/falcon.png" alt=""
+                    width="58" />
+                <span class="font-sans-serif fw-bolder fs-5 d-inline-block">AMPSD</span>
+            </a>
+            <div class="card">
+                <div class="card-body p-4 p-sm-5">
+                    <div class="row flex-between-center mb-2">
+                        <div class="col-auto">
+                            <h5>Log in</h5>
+                        </div>
+                        <div class="col-auto fs--1 text-600">
+                            <span class="mb-0 undefined">or</span> <span><a href="{{ route('register') }}">Create an
+                                    account</a></span>
+                        </div>
+                    </div>
+                    <form method="POST" action="{{ route('login') }}">
+                        @csrf
+                        <div class="mb-3">
+                            <input class="form-control" type="email" name="email" placeholder="Email address" />
+                        </div>
+                        <div class="mb-3">
+                            <input class="form-control" name="password" type="password" placeholder="Password" />
+                        </div>
+                        <div class="row flex-between-center">
+                            <div class="col-auto">
+                                <div class="form-check mb-0"><input class="form-check-input" type="checkbox"
+                                        id="basic-checkbox" checked="checked" /><label class="form-check-label mb-0"
+                                        for="basic-checkbox">Remember
+                                        me</label></div>
+                            </div>
+                            <div class="col-auto"><a class="fs--1" href="{{ route('password.request') }}">Forgot
+                                    Password?</a></div>
+                        </div>
+                        <div class="mb-3"><button class="btn btn-primary d-block w-100 mt-3" type="submit"
+                                name="submit">Log
+                                in</button></div>
+                    </form>
+                    {{-- <div class="position-relative mt-4">
+                        <hr />
+                        <div class="divider-content-center">or log in with</div>
+                    </div>
+                    <div class="row g-2 mt-2">
+                        <div class="col-sm-6"><a class="btn btn-outline-google-plus btn-sm d-block w-100"
+                                href="#"><span class="fab fa-google-plus-g me-2" data-fa-transform="grow-8"></span>
+                                google</a></div>
+                        <div class="col-sm-6"><a class="btn btn-outline-facebook btn-sm d-block w-100" href="#"><span
+                                    class="fab fa-facebook-square me-2" data-fa-transform="grow-8"></span> facebook</a>
+                        </div>
+                    </div> --}}
+                </div>
+            </div>
         </div>
+    </div>
+@endsection
 
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
 
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="current-password" />
-
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
-
-        <!-- Remember Me -->
-        <div class="block mt-4">
-            <label for="remember_me" class="inline-flex items-center">
-                <input id="remember_me" type="checkbox" class="rounded dark:bg-gray-900 border-gray-300 dark:border-gray-700 text-indigo-600 shadow-sm focus:ring-indigo-500 dark:focus:ring-indigo-600 dark:focus:ring-offset-gray-800" name="remember">
-                <span class="ml-2 text-sm text-gray-600 dark:text-gray-400">{{ __('Remember me') }}</span>
-            </label>
-        </div>
-
-        <div class="flex items-center justify-end mt-4">
-            @if (Route::has('password.request'))
-                <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('password.request') }}">
-                    {{ __('Forgot your password?') }}
-                </a>
-            @endif
-
-            <x-primary-button class="ml-3">
-                {{ __('Log in') }}
-            </x-primary-button>
-        </div>
-    </form>
-</x-guest-layout> --}}
-
+{{--
 <!DOCTYPE html>
 <html data-bs-theme="light" lang="en-US" dir="ltr">
 
@@ -184,4 +199,4 @@
   <script src="../../../assets/js/theme.js"></script>
 </body>
 
-</html>
+</html> --}}
